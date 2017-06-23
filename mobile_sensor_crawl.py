f365a2d80c29b3ab3a617d5a0997dc1281c1cc16
gunes acar <gunes.acar@esat.kuleuven.be>
2017-06-23 23:41:54 +0200

    Disable synchronized browsers, use a new profile for each visit.
    Enable JS and CP instrumentations, disable Flash.

--- mobile_sensor_crawl.py
+++ mobile_sensor_crawl.py
@@ -1,4 +1,5 @@
 from automation import TaskManager, CommandSequence
+from tld import get_tld
 
 # The list of sites that we wish to crawl
 NUM_BROWSERS = 15
@@ -18,9 +19,12 @@ manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 
 # Update browser configuration (use this for per-browser settings)
 for i in xrange(NUM_BROWSERS):
-    browser_params[i]['http_instrument'] = True # Record HTTP Requests and Responses
-    browser_params[i]['disable_flash'] = False #Enable flash for all three browsers
-browser_params[0]['headless'] = True #Launch only browser 0 headless
+    browser_params[i]['http_instrument'] = True  # Record HTTP Requests and Responses
+    browser_params[i]['disable_flash'] = True  # Disable flash for all three browsers
+    browser_params[i]['js_instrument'] = True  # Enable JS instrumentation
+    browser_params[i]['cp_instrument'] = True  # Enable content policy instrumentation
+    browser_params[i]['save_javascript'] = True  # save JS files
+    browser_params[i]['headless'] = True  #Launch only browser 0 headless
 
 # Update TaskManager configuration (use this for crawl-wide settings)
 manager_params['data_directory'] = '~/openwpm/'
@@ -32,15 +36,15 @@ manager = TaskManager.TaskManager(manager_params, browser_params)
 
 # Visits the sites with all browsers simultaneously
 for site in sites:
-    command_sequence = CommandSequence.CommandSequence(site)
+    command_sequence = CommandSequence.CommandSequence(site, reset=True)
 
     # Start by visiting the page
-    command_sequence.get(sleep=0, timeout=60)
-
+    command_sequence.get(sleep=10, timeout=60)
+    command_sequence.save_screenshot(get_tld(site) + '_screenshot')
     # dump_profile_cookies/dump_flash_cookies closes the current tab.
     command_sequence.dump_profile_cookies(120)
 
-    manager.execute_command_sequence(command_sequence, index='**') # ** = synchronized browsers
+    manager.execute_command_sequence(command_sequence) # ** = synchronized browsers
 
 # Shuts down the browsers and waits for the data to finish logging
 manager.close()
