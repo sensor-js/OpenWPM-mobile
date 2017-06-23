148b182d8c659d63d620080457541f2688faddb5
gunes acar <gunes.acar@esat.kuleuven.be>
2017-06-24 01:47:41 +0200

    Update sensor test to match the new javascript table format .

--- test/test_sensors.py
+++ test/test_sensors.py
@@ -1,35 +1,28 @@
-import pytest # NOQA
-import os
 import utilities
-import expected
 from openwpmtest import OpenWPMTest
 from ..automation import TaskManager
+from ..automation.utilities import db_utils
+import json
 # TODO: add test for setter instrumentation
 
 
 class TestExtension(OpenWPMTest):
     NUM_BROWSERS = 1
 
-    def get_config(self, data_dir):
-        manager_params, browser_params = TaskManager.load_default_params(self.NUM_BROWSERS)
-        manager_params['data_directory'] = data_dir
-        manager_params['log_directory'] = data_dir
-        browser_params[0]['headless'] = False
-        browser_params[0]['extension']['enabled'] = True
-        browser_params[0]['extension']['jsInstrument'] = True
-        manager_params['db'] = os.path.join(manager_params['data_directory'],
-                                            manager_params['database_name'])
+    def get_config(self, data_dir=""):
+        manager_params, browser_params = self.get_test_config(data_dir)
+        browser_params[0]['js_instrument'] = True
         return manager_params, browser_params
 
     def test_sensor_probing(self, tmpdir):
         test_url = utilities.BASE_TEST_URL + '/sensors.html'
         db = self.visit(test_url, str(tmpdir))
-        rows = utilities.get_javascript_entries(db, True)
+        rows = db_utils.get_javascript_entries(db, all_columns=True)
         observed_sensor_apis = set()
         expected_apis = set(['deviceorientation', 'devicemotion',
                              'deviceproximity', 'devicelight'])
         for row in rows:
-            if row[10] == 0:
-                observed_sensor_apis.add(row[11])
+            if row[9] == "window.addEventListener":
+                observed_sensor_apis.add(json.loads(row[12])["0"])
                 assert row[3] == test_url
         assert observed_sensor_apis == expected_apis
